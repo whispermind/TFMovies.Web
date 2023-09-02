eb02c3f5f8e65605a759a872ad7481804c6e5f09
Evgeniy Slobodianiuk <evgeniy.slobod9@gmail.com>
2023-09-02 15:53:36 +0000

    Merged PR 389: fix imports
    Related work items: #109, #110, #125, #316, #317, #318, #319, #380, #381, #480, #482, #550, #639, #643

--- src/modules/ForgotPassword/ForgotPassPage/index.tsx
+++ src/modules/ForgotPassword/ForgotPassPage/index.tsx
@@ -2,41 +2,41 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { Typography, Stack } from "@mui/material";
 
-import { ForgotPassForm, IForgotPassForm } from "../ForgotPasswordForm";
+import { ForgotPassForm, IForgotPassForm } from "../forgotPasswordForm";
 import { LogAuthWrapper, Loader } from "../../../common/components";
 import { useForgotPassword } from "../../../common/hooks";
 
 export const ForgotPassPage = () => {
-  const [forgotPasswordReq, { isLoading }] = useForgotPassword();
-  const navigate = useNavigate();
+	const [forgotPasswordReq, { isLoading }] = useForgotPassword();
+	const navigate = useNavigate();
 
-  const heading = "Forgot your Password?";
-  const description = "You need to enter your email to recover your password. We will send an instruction with password recovery to your e-mail.";
+	const heading = "Forgot your Password?";
+	const description = "You need to enter your email to recover your password. We will send an instruction with password recovery to your e-mail.";
 
-  const onSubmit = useCallback(
-    async ({ email }: IForgotPassForm) => {
-      try {
-        await forgotPasswordReq(email).unwrap();
-        navigate("/");
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    [forgotPasswordReq, navigate]
-  );
+	const onSubmit = useCallback(
+		async ({ email }: IForgotPassForm) => {
+			try {
+				await forgotPasswordReq(email).unwrap();
+				navigate("/");
+			} catch (e) {
+				console.log(e);
+			}
+		},
+		[forgotPasswordReq, navigate]
+	);
 
-  return isLoading ? (
-    <Loader />
-  ) : (
-    <LogAuthWrapper maxWidth="50%">
-      <Stack
-        rowGap={1.5}
-        mb={7.5}
-      >
-        <Typography variant="Sector">{heading}</Typography>
-        <Typography variant="HBody">{description}</Typography>
-      </Stack>
-      <ForgotPassForm onSubmit={onSubmit} />
-    </LogAuthWrapper>
-  );
+	return isLoading ? (
+		<Loader />
+	) : (
+		<LogAuthWrapper maxWidth="50%">
+			<Stack
+				rowGap={1.5}
+				mb={7.5}
+			>
+				<Typography variant="Sector">{heading}</Typography>
+				<Typography variant="HBody">{description}</Typography>
+			</Stack>
+			<ForgotPassForm onSubmit={onSubmit} />
+		</LogAuthWrapper>
+	);
 };

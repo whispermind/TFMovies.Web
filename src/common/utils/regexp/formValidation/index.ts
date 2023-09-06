57159aea647953290aad9a9f6cbd74f32a678b81
whispermind <h4vefun.dieyoung@gmail.com>
2023-09-07 00:42:58 +0300

    fix/update email regexp

--- src/common/utils/regexp/formValidation/index.ts
+++ src/common/utils/regexp/formValidation/index.ts
@@ -1,5 +1,5 @@
 export const formValidation = {
-	email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+	email: /^[\w-+.]+@([\w-]+\.)+[\w-]{2,4}$/g,
 	nickname: /^[a-zA-Z]+$/g,
 	password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-_+=])[A-Za-z\d-_+=]{8,}$/
 };

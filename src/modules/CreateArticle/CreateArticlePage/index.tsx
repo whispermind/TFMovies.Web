3ce9ade9bdcddeeb29684a88e32e8c0ffe4c8b27
whispermind <h4vefun.dieyoung@gmail.com>
2023-09-26 02:49:53 +0300

    feat/formatt article creation tags string

--- src/modules/CreateArticle/CreateArticlePage/index.tsx
+++ src/modules/CreateArticle/CreateArticlePage/index.tsx
@@ -13,15 +13,14 @@ export const CreateArticlePage = () => {
 
 	const onSubmit = useCallback(
 		async ({ attachment, tags, ThemeId, title, HtmlContent }: ICreateArticleFormSubmit) => {
-			if (attachment) {
-				try {
-					const articleData = { coverImageUrl: attachment, tags: tags.split(" "), ThemeId, title, HtmlContent };
-					await createArticleReq(articleData).unwrap();
-					navigate("/createarticle/success");
-					enqueueSnackbar(snackBarMessages.articleCreated, { variant: "success" });
-				} catch (e) {
-					// handled by middleware
-				}
+			try {
+				const optimizedTags = Array.from(new Set(tags.replaceAll(",", "").split(" ")));
+				const articleData = { coverImageUrl: attachment, tags: optimizedTags, ThemeId, title, HtmlContent };
+				await createArticleReq(articleData).unwrap();
+				navigate("/createarticle/success");
+				enqueueSnackbar(snackBarMessages.articleCreated, { variant: "success" });
+			} catch (e) {
+				// handled by middleware
 			}
 		},
 		[createArticleReq, navigate]

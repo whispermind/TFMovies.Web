337f4695d762889a7d96761184f7b651f249c526
whispermind <h4vefun.dieyoung@gmail.com>
2023-10-05 03:39:05 +0300

    fix/remove autofocus from autocomplete list

--- src/modules/Article/CreateArticle/CreateArticleForm/ThemeAutocomplete/index.tsx
+++ src/modules/Article/CreateArticle/CreateArticleForm/ThemeAutocomplete/index.tsx
@@ -1,10 +1,10 @@
 import { SyntheticEvent, useCallback, useMemo } from "react";
-import { Autocomplete } from "@mui/material";
 import { Control } from "react-hook-form";
+import { Autocomplete } from "@mui/material";
 
 import { useGetThemesQuery, IGetThemeResponseData } from "../../../../../app/api/Themes";
 import { withController } from "../../../../../common/hocs";
-import * as Styled from "../styled";
+import { TextField } from "../styled";
 
 import type { ICreateArticleForm, IStyledInputProps } from "../..";
 
@@ -16,26 +16,26 @@ interface IThemeAutocompleteProps {
 
 export const ThemeAutocomplete = ({ onChange: onThemeChange, control, value }: IThemeAutocompleteProps) => {
 	const { data } = useGetThemesQuery();
-
 	const themes = useMemo(() => data?.map(({ name }) => name), [data]) || [];
 
 	const onChange = useCallback(
-		(_: SyntheticEvent, themeName: string | null) => {
+		(_: SyntheticEvent, themeName: unknown) => {
 			const theme = data?.find(({ name }) => name === themeName);
 			if (theme) onThemeChange(theme);
 		},
 		[onThemeChange, data]
 	);
 
-	const ThemesField = withController<ICreateArticleForm, IStyledInputProps>(Styled.TextField);
+	const ThemesField = withController<ICreateArticleForm, IStyledInputProps>(TextField);
 
 	return (
 		<Autocomplete
 			options={themes}
 			popupIcon={false}
 			onChange={onChange}
+			defaultValue={themes[0] || ""}
 			value={value}
-			isOptionEqualToValue={(option, currentValue) => option === currentValue || value === ""}
+			isOptionEqualToValue={(option, currentValue) => option === currentValue}
 			renderInput={(props) => (
 				<ThemesField
 					{...props}
